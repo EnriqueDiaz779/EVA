8e75e89d63476bc8a82fc198078374c32161f513
michasion0908 <michasion0908@gmail.com>
2026-06-15 11:27:48 -0600

    Actualizar EVA para despliegue

--- miappEVA/eva_mobile/lib/config/app_config.dart
+++ miappEVA/eva_mobile/lib/config/app_config.dart
@@ -1,6 +1,3 @@
 class AppConfig {
-  static const String apiBaseUrl = String.fromEnvironment(
-    'API_BASE_URL',
-    defaultValue: 'http://192.168.0.161:8000',
-  );
+  static const String baseUrl = 'https://eva-web.onrender.com';
 }
