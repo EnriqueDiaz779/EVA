8e88ccb9aa4163f91424ca819d64debde709d896
michasion0908 <michasion0908@gmail.com>
2026-06-15 15:23:49 -0600

    Actualizar EVA para despliegue

--- miappEVA/eva_mobile/lib/config/app_config.dart
+++ miappEVA/eva_mobile/lib/config/app_config.dart
@@ -1,4 +1,4 @@
 class AppConfig {
-  static const String baseUrl = 'https://eva-web.onrender.com';
+  static const String baseUrl = 'https://eva-z50q.onrender.com';
   static const String apiBaseUrl = baseUrl;
 }
